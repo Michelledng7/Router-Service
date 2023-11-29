aba745bc538eda18578e35898a59e2078a885747
Michelle <michelledng7@gmail.com>
2023-11-30 01:43:33 +1100

    add root route and static index file

--- server.js
+++ server.js
@@ -2,26 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+const corsOptions = require('./config/corsOptions')
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
-//const employeeRouter = require('./routes/api/employee')
 
 // custom middleware logger
 app.use(logger);
-
-// Cross Origin Resource Sharing
-const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    optionsSuccessStatus: 200
-}
 app.use(cors(corsOptions));
 
 // built-in middleware to handle urlencoded data
@@ -36,49 +23,10 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 //app.use('/', express.) how to refer routeremployee in serverjs
 
-
-
-app.get('/index(.html)?', (req, res) => {
-    //res.sendFile('./views/index.html', { root: __dirname });
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, '/new-page.html'); //302 by default
-});
-
+//routes
+app.use('/', require('./routes/root'))
 // Route handlers for api
 app.use('/employees', require('./routes/api/employee'))
-
-app.get('/hello(.html)?', (req, res, next) => {
-    console.log('attempted to load hello.html');
-    next()
-}, (req, res) => {
-    res.send('Hello World!');
-});
-
-
-// chaining route handlers
-const one = (req, res, next) => {
-    console.log('one');
-    next();
-}
-
-const two = (req, res, next) => {
-    console.log('two');
-    next();
-}
-
-const three = (req, res) => {
-    console.log('three');
-    res.send('Finished!');
-}
-
-app.get('/chain(.html)?', [one, two, three]);
 
 app.all('*', (req, res) => {
     res.status(404);
